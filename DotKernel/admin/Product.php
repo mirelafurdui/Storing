3922691753aefe9a793040329dc11dee4bfd8f9a
Mirela <furdui1mirela@gmail.com>
2017-08-07 16:41:29 +0300

    project update

---
+++ DotKernel/admin/Product.php
@@ -0,0 +1,36 @@
+<?php
+class Product extends Dot_Model
+{
+	// constructor function
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	// function that joins 2 tables with the origin table "product" and lists them all with some rows changed
+	public function getProductList($page=1)
+	{
+		$select=$this->db->select()
+						 ->from('product')
+						 ->join('category', 'category.id = product.idCategory',['categoryName'=>'name'])
+						 ->join('brand', 'brand.id = product.idBrand',['brandName'=>'name']);
+
+		$dotPaginator = new Dot_Paginator($select,$page,$this->settings->resultsPerPage);
+		return $dotPaginator->getData();
+	}
+	// function that shows product by id and joins with db category and brand
+	public function getProductById($id)
+	{
+		$select=$this->db->select()
+						 ->from('product')
+						 ->where('product.id= ?',$id)
+						 ->joinLeft('category', 'category.id = product.idCategory',['categoryName'=>'name'])
+						 ->joinLeft('brand', 'brand.id = product.idBrand',['brandName'=>'name']);
+		$result=$this->db->fetchRow($select);
+		return $result;
+	}
+	// function that adds a product in the table
+	public function addProduct($data)
+	{
+		$this->db->insert('product',$data);
+	}
+}
