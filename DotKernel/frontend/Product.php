5727a69c5308f6d85ebe5239483ec1c46a7a4b3b
Florin <flo_tzone@yahoo.com>
2017-08-09 17:02:31 +0300

    Added City and Address columns to both registry and update tpl,removed sidemenu,added product controller to frontend, homepage now lists products and single products on click, tpl files are made to look ok, When you press store the text will be bolded, completed about page and now shows in bold when pressed

---
+++ DotKernel/frontend/Product.php
@@ -0,0 +1,33 @@
+<?php
+class Product extends Dot_Model
+{
+	// constructor function
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	// function that joins 2 tables with the origin table "product" and lists them all with some rows changed
+	public function getProductList($page=1)
+	{
+		$select=$this->db->select()
+						 ->from('product')
+						 ->join('category', 'category.id = product.idCategory',['categoryName'=>'name'])
+						 ->join('brand', 'brand.id = product.idBrand',['brandName'=>'name']);
+
+		$dotPaginator = new Dot_Paginator($select,$page,$this->settings->resultsPerPage=20);
+		return $dotPaginator->getData();
+	}
+
+	// function that shows product by id and joins with db category and brand
+	public function getProductById($id)
+	{
+		$select=$this->db->select()
+						 ->from('product')
+						 ->where('product.id= ?',$id)
+						 ->join('category', 'category.id = product.idCategory',['categoryName'=>'name'])
+						 ->join('brand', 'brand.id = product.idBrand',['brandName'=>'name']);
+		$result=$this->db->fetchRow($select);
+		return $result;
+	}
+}
